1563327e3ecd3cadd149bbb372dd4bc773df9dd2
David Esner <david@keboola.com>
2023-02-09 14:51:27 +0100

    print value

--- src/component.py
+++ src/component.py
@@ -21,7 +21,7 @@ class Component(ComponentBase):
         return [
             {"label": 'Joe', "value": 'joe'},
             {"label": 'Doe', "value": 'doe'},
-            {"label": 'Loaded from config parameters', "value": self.configuration.parameters.get(KEY_TEST_CONNECTION)}
+            {"label": 'Loaded from config parameters', "value": self.configuration.parameters.get('test_value')}
         ]
 
     @sync_action('testConnection')
